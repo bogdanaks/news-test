b15830eb95e556850c3dffc69c28c3bc310dff9f
bogdanaks <bogdanaks@bk.ru>
2020-07-18 22:25:27 +0300

    Auth methods

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    auth: true
   },
   mutations: {
+    setAuth(state, value) {
+      state.auth = value
+      localStorage.setItem('auth', value)
+    }
   },
   actions: {
+    setAuth({commit}, value) {
+      commit('setAuth', value)
+    }
   },
-  modules: {
+  getters: {
+    isAuthenticated(state) {
+      return state.auth
+    }
   }
 })
