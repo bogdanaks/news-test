6fed1231ff4dc7f5a86048f970367c88dc875d4a
bogdanaks <bogdanaks@bk.ru>
2020-07-18 22:06:24 +0300

    Add base template

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container loginContainer">
+        <div class="row">
+            <form class="col s4 offset-s4">
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="username" type="text" class="validate">
+                    <label for="username">Username</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                <input id="password" type="password" class="validate">
+                <label for="password">Password</label>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .loginContainer {
+        padding-top: 15%;
+    }
+</style>
