a452277424400810e8aa7391602395a8ec1469ef
bogdanaks <bogdanaks@bk.ru>
2020-07-18 21:59:00 +0300

    Add content

--- src/views/News.vue
+++ src/views/News.vue
@@ -22,6 +22,50 @@
         </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col s12">
+        <div class="card">
+            <div class="card-image">
+                <img src="@/assets/sample-1.jpg">
+                <span class="card-title">News 2</span>
+            </div>
+            <div class="card-content">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Praesentium tempore impedit facilis pariatur, ratione vel 
+                    consectetur explicabo dolore deserunt, esse repudiandae 
+                    obcaecati eaque saepe aspernatur vitae corrupti tenetur 
+                    voluptatibus culpa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Praesentium tempore impedit facilis pariatur, ratione vel 
+                    consectetur explicabo dolore deserunt, esse repudiandae 
+                    obcaecati eaque saepe aspernatur vitae corrupti tenetur 
+                    voluptatibus culpa!
+                </p>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+        <div class="card">
+            <div class="card-image">
+                <img src="@/assets/sample-1.jpg">
+                <span class="card-title">News 3</span>
+            </div>
+            <div class="card-content">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Praesentium tempore impedit facilis pariatur, ratione vel 
+                    consectetur explicabo dolore deserunt, esse repudiandae 
+                    obcaecati eaque saepe aspernatur vitae corrupti tenetur 
+                    voluptatibus culpa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Praesentium tempore impedit facilis pariatur, ratione vel 
+                    consectetur explicabo dolore deserunt, esse repudiandae 
+                    obcaecati eaque saepe aspernatur vitae corrupti tenetur 
+                    voluptatibus culpa!
+                </p>
+            </div>
+        </div>
+        </div>
+    </div>
   </div>
 </template>
 
