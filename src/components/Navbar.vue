961879729de5becee795371d5fd8821356d297b0
bogdanaks <bogdanaks@bk.ru>
2020-07-18 18:22:12 +0300

    Create navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="teal darken-1">
+        <div class="nav-wrapper container">
+            <router-link to='/' class="brand-logo">News</router-link>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <router-link to="/news" active-class="active" tag="li" exact><a>News</a></router-link>
+                <router-link to="/profile" active-class="active" tag="li" exact><a>Profile</a></router-link>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
