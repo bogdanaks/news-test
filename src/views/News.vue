1d2ff2503053f983187aa750238e3524fb81d020
bogdanaks <bogdanaks@bk.ru>
2020-07-18 21:56:14 +0300

    Create News view

---
+++ src/views/News.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+      <div class="row">
+        <div class="col s12">
+        <div class="card">
+            <div class="card-image">
+                <img src="@/assets/sample-1.jpg">
+                <span class="card-title">News 1</span>
+            </div>
+            <div class="card-content">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Praesentium tempore impedit facilis pariatur, ratione vel 
+                    consectetur explicabo dolore deserunt, esse repudiandae 
+                    obcaecati eaque saepe aspernatur vitae corrupti tenetur 
+                    voluptatibus culpa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Praesentium tempore impedit facilis pariatur, ratione vel 
+                    consectetur explicabo dolore deserunt, esse repudiandae 
+                    obcaecati eaque saepe aspernatur vitae corrupti tenetur 
+                    voluptatibus culpa!
+                </p>
+            </div>
+        </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .card-image {
+        height: 150px;
+        overflow: hidden;
+    }
+</style>
