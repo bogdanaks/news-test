fb5ca8d530ac5c35a1011d211979399be3604a5a
bogdanaks <bogdanaks@bk.ru>
2020-07-18 22:55:21 +0300

    Add methods submit form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="container loginContainer">
         <div class="row">
-            <form class="col s4 offset-s4">
-            <div class="row">
-                <div class="input-field col s12">
-                    <input id="username" type="text" class="validate">
-                    <label for="username">Username</label>
+            <form class="col s4 offset-s4" @submit.prevent="submitHandler">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="username" type="text" class="validate" v-model="user.username">
+                        <label for="username">Username</label>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                <input id="password" type="password" class="validate">
-                <label for="password">Password</label>
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input id="password" type="password" class="validate"  v-model="user.password">
+                    <label for="password">Password</label>
+                    </div>
                 </div>
-            </div>
+                <div class="row">
+                    <button type="submit" class="waves-effect waves-light btn right">Login</button>
+                </div>
             </form>
         </div>
     </div>
@@ -21,7 +24,19 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            user: {
+                username: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        submitHandler: function() {
+            this.$store.dispatch('setAuth', this.user)
+        },
+    }
 }
 </script>
 
