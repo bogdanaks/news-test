fea5c2d80426b3cd402ba41f1a2d882aee37701d
bogdanaks <bogdanaks@bk.ru>
2020-07-18 23:11:21 +0300

    Add exit link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,7 @@
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <router-link to="/news" active-class="active" tag="li" exact><a>News</a></router-link>
                 <router-link to="/profile" active-class="active" tag="li" exact><a>Profile</a></router-link>
+                <li @click="exitHandler" v-if="this.$store.getters.isAuthenticated === 'true'"><a>Exit</a></li>
             </ul>
         </div>
     </nav>
@@ -12,7 +13,12 @@
 
 <script>
 export default {
-
+    methods: {
+        exitHandler: function() {
+            this.$store.dispatch('setAuth', false)
+            this.$router.push('/')
+        }
+    }
 }
 </script>
 
